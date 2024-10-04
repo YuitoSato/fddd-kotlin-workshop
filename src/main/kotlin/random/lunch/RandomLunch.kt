851a7f2f65e1b94277a4b2561ea7fcb0f458ae43
eiichi.mita <eiichi.mita@loglass.co.jp>
2024-10-04 14:30:32 +0900

    2024/10/4

--- src/main/kotlin/random/lunch/RandomLunch.kt
+++ src/main/kotlin/random/lunch/RandomLunch.kt
@@ -1,5 +1,6 @@
 package com.example.random.lunch
 
+import com.github.michaelbull.result.Err
 import java.time.LocalDate
 import com.github.michaelbull.result.Result
 
@@ -26,8 +27,17 @@ enum class EmployeeType {
   INTERN,
 }
 
+typealias Email = String
+
 // 参加者
 data class Participant(
+  val id: ParticipantId,
+  val name: String,
+  val email: String,
+  val employeeType: EmployeeType,
+)
+
+data class Employee(
   val id: ParticipantId,
   val name: String,
   val email: String,
@@ -44,6 +54,17 @@ data class DepartmentId(
 data class Department(
   val id: DepartmentId,
   val name: String
+)
+
+// 検証済みのランダムランチリクエスト
+data class VerifiedRandomLunchRequest(
+  val id: Int,
+  val participantIds: List<ParticipantId>,
+  val date: LocalDate,
+  val applicationDate: LocalDate,
+  val applicationBy: Participant,
+  val amount: Int,
+  val invoiceUrl: String,
 )
 
 // 承認されたランダムランチリクエストID
@@ -83,3 +104,40 @@ data class RandomLunchHistory(
   val invoiceUrl: String,
   val approvedRandomLunchRequestId: ApprovedRandomLunchRequestId,
 )
+
+fun main() {
+  val param = InitRandomLunchRequest(
+    id = 1,
+    participantIds = listOf(ParticipantId(1), ParticipantId(2)),
+    date = LocalDate.now(),
+    applicationDate = LocalDate.now(),
+    applicationBy = Participant(
+      id = ParticipantId(1),
+      name = "name",
+      email = "email",
+      employeeType = EmployeeType.FULL_TIME,
+      departmentIds = listOf(DepartmentId(1))
+    ),
+    amount = 1000,
+    invoiceUrl = "url"
+  )
+   val getEmployee: (Email) -> Employee = { mailAddres ->
+     Employee()
+   }
+}
+
+fun verifyRandomLunchRequest(
+  param: InitRandomLunchRequest,
+  getEmployee: (Email) -> Employee,
+  getDepartment: ()
+): Result<VerifiedRandomLunchRequest, DeniedRandomLunchRequest> {
+  if (param.participantIds.size > 6) {
+    return Err(DeniedRandomLunchRequest(param.id, DeniedReason.Participants))
+  }
+  TODO()
+}
+
+fun saveParticipants(approvedRandomLunch: ApproveRandomLunch, saveFun: () -> Result<Unit, RuntimeException>): Unit {
+
+}
+
